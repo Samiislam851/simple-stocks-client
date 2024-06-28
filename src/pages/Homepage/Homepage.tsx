2b1a088e3c549f2a4077ea94afe387dc17cb3f2d
samiislam851 <samisiam851@gmail.com>
2024-06-28 22:45:51 +0600

    Table done with editable functionalities

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -4,11 +4,23 @@ import StocksTable from '../../components/StocksTable/StocksTable'
 import './Hompage.css'
 import axios from 'axios';
 
-
 const Homepage = () => {
 
-  const [stocksData, setStocksData] = useState([]);
+  const [stocksData, setStocksData] = useState<any>([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/stock_market_data.json');
+        const data = await response.json();
+        setStocksData(data);
+      } catch (error) {
+        console.error('Error fetching the data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='max-w-full h-full w-full  bg-transparent pt-5 p-10 rounded-lg text-gray-700  '>
@@ -19,7 +31,7 @@ const Homepage = () => {
         {/* graph */}
       </div>
       <div>
-        <StocksTable />
+        <StocksTable setStocksData={setStocksData}  stocksData={stocksData} />
       </div>
     </div>
   )
